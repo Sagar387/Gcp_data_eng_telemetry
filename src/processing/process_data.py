c279044d86d980dc3439e9f55cae8f44cf27f2d9
Sagar387 <144396579+Sagar387@users.noreply.github.com>
2026-02-16 02:23:37 -0500

    Bronze layer

---
+++ src/processing/process_data.py
@@ -0,0 +1,69 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_json, to_timestamp, avg, count, when
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
+
+
+# Configuration
+BUCKET_NAME = "telemetry-lake-sagar"
+PROJECT_ID = "pulse-stream-core"
+
+def main():
+    spark = SparkSession.builder \
+        .appName("Pulse-Stream-Refinery") \
+        .getOrCreate()
+    print("Spark Session Started!")
+    
+    # Define schema 
+    schema = StructField([
+        StructField("event_uuid", StringType(), True),
+        StructField("athlete_id", StringType(), True),
+        StructField("timestamp", StringType(), True),
+        StructField("heart_rate", DoubleType(), True),
+        StructField("speed", DoubleType(), True),
+        StructField("location", StringType(), True),
+        
+    ])
+        
+    # Read the telemetry data (Data swamp currently)
+    # Read the raw json files in the cloud storage bucket
+    raw_df = spark.read \
+        .schema(schema) \
+        .json(f"gs://{"telemetry-lake-sagar"}/raw_telemetry/*.json")
+    
+    print(f"Ingested {raw_df.count()} raw records.")
+    
+    # Read the Reference Json file in Cloud Storage
+    athlete_df = spark.read \
+        .option("multiline", "true") \
+        .json(f"gs://{"telemetry-lake-sagar"}/reference_data/athelte.json")
+        
+    # Data Cleaning Bronze layer
+    
+    # Dropping duplicates
+    clean_df = raw_df.dropDuplicates(["event_uuid"])
+    
+    # Handle NUll (sensor errors)
+    clean_df = clean_df.filter(col("heart_rate").isNotNull())
+    
+    # Clean up outliers (heart rate over 220)
+    clean_df = clean_df.withColumn("heart_rate", when(col("heart_rate")>220, 220).otherwise(col("heart_rate")))
+    
+    # Adding names and countries to the telemetry
+    # Joining with the same athlete id
+    final_df = clean_df.join(athlete_df, "athelte_id","left")
+    
+    # Checking
+    final_df.select("name", "country", "heart_rate", "speed", "times_stamp").show(5)
+    
+    # Write : save this to silver layer (parquet format as it is faster than json)
+    final_df.write \
+        .mode("overwrite") \
+        .parquet(f"gs://{"telemetry-lake-sagar"}/silver_telemetry/")
+        
+    print("Data has successfully been writen to silver layer")
+    
+    #End spark session
+    spark.stop()
+    
+if __name__ == "__main__":
+    main()
